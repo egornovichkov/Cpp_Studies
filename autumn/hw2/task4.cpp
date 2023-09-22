fe17df648ae9521c12b250d1d9e6633615a45702
Новичков Егор <novichkovegor41@gmail.com>
2023-09-22 22:03:42 +0300

    hw2 corrections done

--- autumn/hw2/task4.cpp
+++ autumn/hw2/task4.cpp
@@ -1,51 +1,38 @@
 #include <iostream>
-#include <string>
+#include <algorithm>
 
-void lcsAlgo(std::string S1, std::string S2, int m, int n) {
-    int LCS_table[m + 1][n + 1];
-    for (int i = 0; i <= m; i++) {
-        for (int j = 0; j <= n; j++) {
-            if (i == 0 || j == 0) {
-                LCS_table[i][j] = 0;
-            } else if (S1[i - 1] == S2[j - 1]) {
-                LCS_table[i][j] = LCS_table[i - 1][j - 1] + 1;
-            } else {
-                LCS_table[i][j] = std::max(LCS_table[i - 1][j], LCS_table[i][j - 1]);
-            }
+int main()
+{
+    int size1;
+    int elem;
+    std::cout << "Input size of array: ";
+    std::cin >> size1;
+    int *ptr1 = new int[size1];
+    std::cout << "Input elements of array:\n";
+    for (int i = 0; i < size1; ++i)
+    {
+        std::cin >> elem;
+        ptr1[i] = elem;
+    }
+    int size2;
+    std::cout << "Input new size of array: ";
+    std::cin >> size2;
+    int *ptr2 = new int[size2];
+    for (int i = 0; i < std::min(size1, size2); ++i)
+        ptr2[i] = ptr1[i];
+    if (size2 > size1)
+    {
+        std::cout << "Add more elements to array: ";
+        for (int i = 0; i < size2 - size1; ++i)
+        {
+            std::cin >> elem;
+            ptr2[size1 + i] = elem;
         }
     }
+    int sum = 0;
+    for (int i = 0; i < size2; ++i)
+        sum += ptr2[i];
+    std::cout << "Sum of elements is: " << sum;
 
-    int index = LCS_table[m][n];
-    char lcs[index + 1];
-    lcs[index] = '\0';
-
-    int i = m, j = n;
-    while (i > 0 && j > 0) {
-        if (S1[i - 1] == S2[j - 1]) {
-            lcs[index - 1] = S1[i - 1];
-            --i;
-            --j;
-            --index;
-        } else if (LCS_table[i - 1][j] > LCS_table[i][j - 1]) {
-            --i;
-        } else {
-            --j;
-        }
-    }
-    std::cout << "S1 : " << S1 << "\nS2 : " << S2 << "\nLCS: " << lcs << "\n";
-}
-
-int main() {
-
-    setlocale(LC_ALL, "");
-    std::string S1;
-    std::string S2;
-    std::wcout << L"Введите первую строку: ";
-    std::cin >> S1;
-    std::wcout << L"Введите вторую строку: ";
-    std::cin >> S2;
-
-    int m = S1.length();
-    int n = S2.length();
-    lcsAlgo(S1, S2, m, n);
+    return 0;
 }
