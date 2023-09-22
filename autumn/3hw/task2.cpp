60b978c5425bdf6cd331e0ad4d9f240b13ab6da1
Новичков Егор <novichkovegor41@gmail.com>
2023-09-22 17:53:40 +0300

    3hw task2 done

--- autumn/3hw/task2.cpp
+++ autumn/3hw/task2.cpp
@@ -3,75 +3,63 @@
 
 using vector = std::vector<int>;
 
-void array_merge(vector &arr, vector &left_arr, vector &right_arr, bool mode);
-void merge_sort(vector &arr, bool mode);
+void array_merge(vector &arr, int start, int end, bool mode);
+void merge_sort(vector &arr, int start, int end, bool mode);
 
-void merge_sort(vector &arr, bool mode)
+void merge_sort(vector &arr, int start, int end, bool mode)
 {
-    if (arr.size() <= 1)
+    if (start < end)
     {
-        return;
+        int mid = (start + end) / 2;
+        merge_sort(arr, start, mid, mode);
+        merge_sort(arr, mid + 1, end, mode);
+        array_merge(arr, start, end, mode);
     }
-    vector left_arr;
-    vector right_arr;
-    int mid = arr.size() / 2;
-    for (auto i = 0; i < mid; ++i)
-        left_arr.push_back(arr[i]);
-    for (auto j = mid; j >= mid; ++j)
-        right_arr.push_back(arr[j]);
-    // merge_sort(left_arr, mode);
-    // merge_sort(right_arr, mode);
-    array_merge(arr, left_arr, right_arr, mode);
     return;
 }
 
-void array_merge(vector &arr, vector &left_arr, vector &right_arr, bool mode)
+void array_merge(vector &arr, int start, int end, bool mode)
 {
-    vector temp_arr;
+    vector temp_arr = arr;
+    int mid = (start + end) / 2;
+    int left_first = start;    // Индекс первого элемента левой половины
+    int right_first = mid + 1; // Индекс первого элемента правой половины
     if (mode == 0)
     {
-        while ((left_arr.size() > 0) & (right_arr.size() > 0))
+        for (int i = left_first; i <= end; ++i)
         {
-            if (left_arr.front() > right_arr.front())
+            // Левая часть вектора левее середины и (либо правая половина вектора пустая, либо первый элемент левого вектора меньше 
+            // первого элемента правого вектора)
+            if ((left_first <= mid) && ((right_first > end) || (arr[left_first] < arr[right_first]))) 
             {
-                temp_arr.push_back(left_arr.front());
-                left_arr.erase(left_arr.cbegin());
+                temp_arr[i] = arr[left_first];
+                ++left_first;
             }
             else
             {
-                temp_arr.push_back(right_arr.front());
-                right_arr.erase(right_arr.cbegin());
+                temp_arr[i] = arr[right_first];
+                ++right_first;
             }
         }
     }
     else
     {
-        while ((left_arr.size() > 0) & (right_arr.size() > 0))
+        for (int i = left_first; i <= end; ++i)
         {
-            if (left_arr.front() < right_arr.front())
+            if ((left_first <= mid) && ((right_first > end) || (arr[left_first] > arr[right_first])))
             {
-                temp_arr.push_back(left_arr.front());
-                left_arr.erase(left_arr.cbegin());
+                temp_arr[i] = arr[left_first];
+                ++left_first;
             }
             else
             {
-                temp_arr.push_back(right_arr.front());
-                right_arr.erase(right_arr.cbegin());
+                temp_arr[i] = arr[right_first];
+                ++right_first;
             }
         }
     }
 
-    while (left_arr.size() > 0)
-    {
-        temp_arr.push_back(left_arr.front());
-        left_arr.erase(left_arr.cbegin());
-    }
-    while (right_arr.size() > 0)
-    {
-        temp_arr.push_back(right_arr.front());
-        left_arr.erase(right_arr.cbegin());
-    }
-    for (auto i : arr)
+    for (int i = start; i <= end; ++i)
         arr[i] = temp_arr[i];
     return;
 }
@@ -79,23 +67,24 @@ void array_merge(vector &arr, vector &left_arr, vector &right_arr, bool mode)
 int main()
 {
     setlocale(LC_ALL, "");
-    char a;
     vector arr;
     bool mode;
-    std::wcout << L"Введите массив, который хотите отсортировать (чтобы закончить ввод массива напишите q)\n";
-    std::cin >> a;
-    while (a != 'q')
+    int n;
+    int a;
+    std::wcout << L"Введите длину массива ";
+    std::cin >> n;
+    std::wcout << L"Введите значения в массив\n";
+    for (int i = 0; i < n; ++i)
     {
-        int n = a - '0';
-        arr.push_back(n);
         std::cin >> a;
+        arr.push_back(a);
     }
-    std::cin.clear();
     std::wcout << L"Введите режим сортировки: 0 - по возрастанию, 1 - по убыванию\n";
     std::cin >> mode;
     for (auto &j : arr)
         std::cout << j << " ";
-    merge_sort(arr, mode);
+    std::cout << "\n";
+    merge_sort(arr, 0, n - 1, mode);
     for (auto &j : arr)
         std::cout << j << " ";
     return 0;
